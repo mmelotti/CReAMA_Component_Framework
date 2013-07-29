7a866ba86bd3f872d5fd700f5460fc72b6b0b4e2
m_melotti@hotmail.com <m_melotti@hotmail.com>
2013-07-29 19:33:34 -0300

    user layout

--- src/com/gw/android/first_components/my_components/user/UserViewGUI.java
+++ src/com/gw/android/first_components/my_components/user/UserViewGUI.java
@@ -1,7 +1,27 @@
 package com.gw.android.first_components.my_components.user;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gw.android.R;
+import com.gw.android.first_components.my_fragment.CRActivity;
 import com.gw.android.first_components.my_fragment.CRComponent;
 
 public class UserViewGUI extends CRComponent {
 
+	private LayoutInflater li;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		setControlActivity((CRActivity) getActivity());
+
+		li = inflater;
+		View view = inflater.inflate(R.layout.user_view, container, false);
+		
+		return view;
+	}
+	
 }
