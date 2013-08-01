d1d0cd460d70baf4ac8f6d3ba508635db907cccb
m_melotti@hotmail.com <m_melotti@hotmail.com>
2013-08-01 13:34:11 -0300

    UserViewGUI.java +++

--- src/com/gw/android/first_components/my_components/user/UserViewGUI.java
+++ src/com/gw/android/first_components/my_components/user/UserViewGUI.java
@@ -7,8 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.gw.android.R;
+import com.gw.android.components.connection_manager.AsyncRequestHandler;
 import com.gw.android.components.request.Request;
 import com.gw.android.first_components.my_components.faq.Faq;
 import com.gw.android.first_components.my_fragment.CRActivity;
@@ -17,19 +19,21 @@ import com.gw.android.first_components.my_fragment.CRComponent;
 public class UserViewGUI extends CRComponent {
 
 	private LayoutInflater li;
-	private String url, urlView;
+	private String urlView;
 	int userUrl;
 	private boolean conectado = true;
+	private TextView login, name, email,id;
 
-	
+	String ip = "200.137.66.94";
+	String url = "http://" + ip
+			+ ":8080/GW-Application-Arquigrafia/groupware-workbench";
+
 	private String getUrl() {
-		SharedPreferences testPrefs = getActivity()
-				.getApplication()
+		SharedPreferences testPrefs = getActivity().getApplication()
 				.getSharedPreferences("test_prefs", Context.MODE_PRIVATE);
 		return testPrefs.getString("arquigrafia_base_url", "");
 	}
-	
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -38,12 +42,29 @@ public class UserViewGUI extends CRComponent {
 		li = inflater;
 		View view = inflater.inflate(R.layout.user_view, container, false);
 
-		
-		url = getUrl();
+		// url = getUrl();
 		userUrl = 1;
-		urlView = url + "/faq/" + userUrl + "?_format=json";
-		
+		urlView = url + "/users/" + userUrl + "?_format=json";
+
+		name = (TextView) view.findViewById(R.id.userName);
+		email = (TextView) view.findViewById(R.id.userEmail);
+		login = (TextView) view.findViewById(R.id.userLogin);
+		id = (TextView) view.findViewById(R.id.userId);
+
+		AsyncRequestHandler mHandler = new AsyncRequestHandler() {
+			@Override
+			public void onSuccess(String response) {
+				atualizarAfterSucces();
+			}
+		};
+		setComponentRequestCallback(mHandler);
+
 		return view;
+	}
+
+	@Override
+	protected void onBind() {
+		viewRequest();
 	}
 
 	void viewRequest() {
@@ -66,6 +87,14 @@ public class UserViewGUI extends CRComponent {
 
 	public void preencheCampos() {
 		// preenche com oq ta no cache
+	}
+
+	public void atualizarAfterSucces() {
+		// TODO parser xml
+		name.setText(name.getText() + "Administrador");
+		email.setText(email.getText() + "admin@arquigrafia.com.br");
+		login.setText(login.getText() + "admin");
+		id.setText(id.getText() + "1");
 	}
 
 }
--- src/com/gw/android/first_components/my_fragment/CRComponent.java
+++ src/com/gw/android/first_components/my_fragment/CRComponent.java
@@ -32,7 +32,7 @@ public abstract class CRComponent extends GenericComponent {
 	public void reloadActivity() {
 		Activity a = getActivity();
 		a.finish();
-		startActivity(a.getIntent()); 
+		startActivity(a.getIntent());
 	}
 
 	public interface RequestListener {
@@ -82,107 +82,119 @@ public abstract class CRComponent extends GenericComponent {
 	public void setNick(String nick) {
 		this.nick = nick;
 	}
-	
+
 	public int getCollabletId() {
 		Activity a = getActivity();
-		int resId = a.getResources().getIdentifier(getNick(), "integer", a.getPackageName());
+		int resId = a.getResources().getIdentifier(getNick(), "integer",
+				a.getPackageName());
 		return a.getResources().getInteger(resId);
 	}
 
 	public void enviou() {
-		if(component!=null)
-			sendMessage(MSG_ENVIOU);		
+		if (component != null)
+			sendMessage(MSG_ENVIOU);
 	}
-	
+
 	public List<ComponentSimpleModel> getListSimple(Long target, Activity a) {
 		ArrayList<ComponentSimpleModel> list = new ArrayList<ComponentSimpleModel>();
 
 		return list;
 	}
-	
-	public void submittedFrom(Long target){
-		
+
+	public void submittedFrom(Long target) {
+
 	}
-	
+
 	protected void setComponentRequestCallback(AsyncRequestHandler h) {
 		componentHandler = h;
 	}
-	
+
 	public void setApplicationRequestCallback(AsyncRequestHandler h) {
 		applicationHandler = h;
 	}
-	
+
 	protected void makeRequest(Request request) {
-		getConnectionManager().makeRequest(request, getActivity(), componentHandler, applicationHandler);
+		if(getActivity()==null){
+			
+		}
+		else{
+			getConnectionManager().makeRequest(request, getActivity(),
+					componentHandler, applicationHandler);
+		}
+		
 	}
 
 	// Código de comunicação com o serviço ConnectionManager
 	private ConnectionManager mBoundService;
 	private boolean mIsBound = false;
-	
-	protected ConnectionManager  getConnectionManager() {
+
+	protected ConnectionManager getConnectionManager() {
 		return mBoundService;
 	}
-	 
+
 	// Método chamado quando o componente se conecta ao ConnectionManager
 	protected void onBind() {
-		
+
 	}
 
 	// Método chamado quando o componente se desconecta do ConnectionManager
 	protected void onUnbind() {
-		 
+
 	}
-	
+
 	private ServiceConnection mConnection = new ServiceConnection() {
-	    public void onServiceConnected(ComponentName className, IBinder service) {
-	        // This is called when the connection with the service has been
-	        // established, giving us the service object we can use to
-	        // interact with the service.  Because we have bound to a explicit
-	        // service that we know is running in our own process, we can
-	        // cast its IBinder to a concrete class and directly access it.
-	    	
-	        mBoundService = ((ConnectionManager.LocalBinder)service).getService(); 
-	        onBind();
-	    }
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// This is called when the connection with the service has been
+			// established, giving us the service object we can use to
+			// interact with the service. Because we have bound to a explicit
+			// service that we know is running in our own process, we can
+			// cast its IBinder to a concrete class and directly access it.
 
-	    public void onServiceDisconnected(ComponentName className) {
-	        // This is called when the connection with the service has been
-	        // unexpectedly disconnected -- that is, its process crashed.
-	        // Because it is running in our same process, we should never
-	        // see this happen.
-	        mBoundService = null;
-	        onUnbind();
-	        Toast.makeText(getActivity(), "Desconectado do serviço", Toast.LENGTH_SHORT).show();
-	        // StandOutWindow.close(context, cls, id);
-	    }
+			mBoundService = ((ConnectionManager.LocalBinder) service)
+					.getService();
+			onBind();
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			// This is called when the connection with the service has been
+			// unexpectedly disconnected -- that is, its process crashed.
+			// Because it is running in our same process, we should never
+			// see this happen.
+			mBoundService = null;
+			onUnbind();
+			Toast.makeText(getActivity(), "Desconectado do serviço",
+					Toast.LENGTH_SHORT).show();
+			// StandOutWindow.close(context, cls, id);
+		}
 	};
-	
+
 	void doBindService() {
-	    // Establish a connection with the service.  We use an explicit
-	    // class name because we want a specific service implementation that
-	    // we know will be running in our own process (and thus won't be
-	    // supporting component replacement by other applications).
-	    getActivity().bindService(new Intent(getActivity(), ConnectionManager.class), mConnection, Context.BIND_AUTO_CREATE);
-        mIsBound = true;
-	}
-	
-	void doUnbindService() {
-	    if (mIsBound) {
-	        // Detach our existing connection.
-	        getActivity().unbindService(mConnection);
-	        mIsBound = false;
-	    }
+		// Establish a connection with the service. We use an explicit
+		// class name because we want a specific service implementation that
+		// we know will be running in our own process (and thus won't be
+		// supporting component replacement by other applications).
+		getActivity().bindService(
+				new Intent(getActivity(), ConnectionManager.class),
+				mConnection, Context.BIND_AUTO_CREATE);
+		mIsBound = true;
 	}
 
-	//---------------------------------------------------------------
+	void doUnbindService() {
+		if (mIsBound) {
+			// Detach our existing connection.
+			getActivity().unbindService(mConnection);
+			mIsBound = false;
+		}
+	}
+
+	// ---------------------------------------------------------------
 
 	@Override
 	public void onDestroy() {
-	    super.onDestroy();
-	    doUnbindService();
+		super.onDestroy();
+		doUnbindService();
 	}
-	
+
 	@Override
 	public void onStart() {
 		super.onStart();
