5aae215a4f769a3ce4a4766496f0fbb7073edf01
m_melotti@hotmail.com <m_melotti@hotmail.com>
2013-08-21 13:57:07 -0300

    acertozinho

--- src/com/gw/android/first_components/my_components/photo/PhotoGalleryGUI.java
+++ src/com/gw/android/first_components/my_components/photo/PhotoGalleryGUI.java
@@ -28,59 +28,62 @@ import com.gw.android.first_components.my_fragment.CRComponent;
 
 @SuppressLint("ValidFragment")
 public class PhotoGalleryGUI extends CRComponent {
-	private PhotoDao photoDao; 
+	private PhotoDao photoDao;
 	private Gallery picGallery;
 	private PicAdapter imgAdapt;
 	private TextView description;
 	List<Photo> list;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.image_gallery, container, false);
-		
+
 		picGallery = (Gallery) view.findViewById(R.id.gallery);
 		description = (TextView) view.findViewById(R.id.description);
-		
+
 		photoDao = initPhotoDao(getActivity());
 		list = photoDao.queryBuilder().orderAsc(Properties.Id).list();
 		photoDao.getDatabase().close();
-		
-		Photo photo = list.get(0);
+
+		Photo photo = null;
+		if (!list.isEmpty()) {
+			photo = list.get(0);
+		}
+
 		if (photo != null) {
 			byte[] data = photo.getPhotoBytes();
 			Bitmap bm = BitmapFactory.decodeByteArray(data, 0, data.length);
 		}
-		
-        imgAdapt = new PicAdapter(getActivity());
-        picGallery.setAdapter(imgAdapt);
-        picGallery.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View v,
-                    int position, long id) {
-            	description.setText(list.get(position).getText());
-            	Log.e("view", " " + position);
-            }
+		imgAdapt = new PicAdapter(getActivity());
+		picGallery.setAdapter(imgAdapt);
+		picGallery.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // Do nothing
-            }
-        });
-        
-		/*GridView gridview = (GridView) view.findViewById(R.id.gridview);
-		gridview.setAdapter(new GalleryAdapter(getActivity(), l));
-
-		gridview.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v,
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View v,
 					int position, long id) {
-				Intent trocatela = new Intent(getActivity(), NewListActivity.class);
-				trocatela.putExtra("nImagem", id);
-				getActivity().startActivity(trocatela); 
+				description.setText(list.get(position).getText());
+				Log.e("view", " " + position);
 			}
-		});*/
-		
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// Do nothing
+			}
+		});
+
+		/*
+		 * GridView gridview = (GridView) view.findViewById(R.id.gridview);
+		 * gridview.setAdapter(new GalleryAdapter(getActivity(), l));
+		 * 
+		 * gridview.setOnItemClickListener(new OnItemClickListener() { public
+		 * void onItemClick(AdapterView<?> parent, View v, int position, long
+		 * id) { Intent trocatela = new Intent(getActivity(),
+		 * NewListActivity.class); trocatela.putExtra("nImagem", id);
+		 * getActivity().startActivity(trocatela); } });
+		 */
+
 		return view;
 	}
 
@@ -92,97 +95,104 @@ public class PhotoGalleryGUI extends CRComponent {
 		PhotoDao photoDao = daoMaster.newSession().getPhotoDao();
 		return photoDao;
 	}
-	
+
 	// Classe auxiliar
-    public class PicAdapter extends BaseAdapter {
-    	
-    	//use the default gallery background image
-        int defaultItemBackground;
-        
-        //gallery context
-        private Context galleryContext;
+	public class PicAdapter extends BaseAdapter {
 
-        //array to store bitmaps to display
-        private Bitmap[] imageBitmaps;
+		// use the default gallery background image
+		int defaultItemBackground;
 
-        //constructor
-        public PicAdapter(Context c) {
-        	
-        	//instantiate context
-        	galleryContext = c;
-        	
-        	//create bitmap array
-            imageBitmaps = new Bitmap[list.size()];
-            //decode the placeholder image
-            //placeholder = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+		// gallery context
+		private Context galleryContext;
 
-            //set placeholder as all thumbnail images in the gallery initially
+		// array to store bitmaps to display
+		private Bitmap[] imageBitmaps;
+
+		// constructor
+		public PicAdapter(Context c) {
+
+			// instantiate context
+			galleryContext = c;
+
+			// create bitmap array
+			imageBitmaps = new Bitmap[list.size()];
+			// decode the placeholder image
+			// placeholder = BitmapFactory.decodeResource(getResources(),
+			// R.drawable.ic_launcher);
+
+			// set placeholder as all thumbnail images in the gallery initially
 			for (int i = 0; i < imageBitmaps.length; i++) {
 				Photo photo = list.get(i);
-				
+
 				if (photo != null) {
 					byte[] data = photo.getPhotoBytes();
-					Bitmap bm = BitmapFactory.decodeByteArray(data, 0, data.length);
+					Bitmap bm = BitmapFactory.decodeByteArray(data, 0,
+							data.length);
 					imageBitmaps[i] = bm;
 				}
 			}
-            //get the styling attributes - use default Android system resources
-            TypedArray styleAttrs = galleryContext.obtainStyledAttributes(R.styleable.PicGallery);
-            //get the background resource
-            defaultItemBackground = styleAttrs.getResourceId(
-            		R.styleable.PicGallery_android_galleryItemBackground, 0);
-            //recycle attributes
-            styleAttrs.recycle();
-        }
+			// get the styling attributes - use default Android system resources
+			TypedArray styleAttrs = galleryContext
+					.obtainStyledAttributes(R.styleable.PicGallery);
+			// get the background resource
+			defaultItemBackground = styleAttrs.getResourceId(
+					R.styleable.PicGallery_android_galleryItemBackground, 0);
+			// recycle attributes
+			styleAttrs.recycle();
+		}
 
-        //BaseAdapter methods
-        
-        //return number of data items i.e. bitmap images
-        public int getCount() {
-            return imageBitmaps.length;
-        }
+		// BaseAdapter methods
 
-        //return item at specified position
-        public Object getItem(int position) {
-            return position;
-        }
+		// return number of data items i.e. bitmap images
+		public int getCount() {
+			return imageBitmaps.length;
+		}
 
-        //return item ID at specified position
-        public long getItemId(int position) {
-            return position;
-        }
+		// return item at specified position
+		public Object getItem(int position) {
+			return position;
+		}
 
-        //get view specifies layout and display options for each thumbnail in the gallery
-        public View getView(int position, View convertView, ViewGroup parent) {
+		// return item ID at specified position
+		public long getItemId(int position) {
+			return position;
+		}
 
-        	//create the view
-            ImageView imageView = new ImageView(galleryContext);
-            //specify the bitmap at this position in the array
-            imageView.setImageBitmap(imageBitmaps[position]);
-            //set layout options
-            imageView.setLayoutParams(new Gallery.LayoutParams(400, 300));
-            //scale type within view area
-            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            //set default gallery item background
-            imageView.setBackgroundResource(defaultItemBackground);
-            //return the view
-            return imageView;
-        }
-        
-        //custom methods for this app
-        
-        //helper method to add a bitmap to the gallery when the user chooses one
-        public void addPic(Bitmap newPic)
-        {
-        	//set at currently selected index
-        	imageBitmaps[10] = newPic;
-        }
-        
-        //return bitmap at specified position for larger display
-        public Bitmap getPic(int posn)
-        {
-        	//return bitmap at posn index
-        	return imageBitmaps[posn];
-        }
-    }
+		// get view specifies layout and display options for each thumbnail in
+		// the gallery
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			// create the view
+			ImageView imageView = new ImageView(galleryContext);
+			// specify the bitmap at this position in the array
+			imageView.setImageBitmap(imageBitmaps[position]);
+			// set layout options
+			imageView.setLayoutParams(new Gallery.LayoutParams(200, 180));
+			// 400, 300 fica gigante no meu!
+			// 300, 200 fica legal ateh mas vou diminuir mais
+			// 200. 180 ficou perfeito
+
+			// scale type within view area
+			imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+			// set default gallery item background
+			imageView.setBackgroundResource(defaultItemBackground);
+			// return the view
+			return imageView;
+		}
+
+		// custom methods for this app
+
+		// helper method to add a bitmap to the gallery when the user chooses
+		// one
+		public void addPic(Bitmap newPic) {
+			// set at currently selected index
+			imageBitmaps[10] = newPic;
+		}
+
+		// return bitmap at specified position for larger display
+		public Bitmap getPic(int posn) {
+			// return bitmap at posn index
+			return imageBitmaps[posn];
+		}
+	}
 }
