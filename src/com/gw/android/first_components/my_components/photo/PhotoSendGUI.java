7b776070760a2df6092983162881acb24d55763e
m_melotti@hotmail.com <m_melotti@hotmail.com>
2013-09-02 16:36:55 -0300

    photo send victor

--- src/com/gw/android/first_components/my_components/photo/PhotoSendGUI.java
+++ src/com/gw/android/first_components/my_components/photo/PhotoSendGUI.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -20,17 +21,19 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.gw.android.R;
+import com.gw.android.components.request.Request;
 import com.gw.android.first_components.my_components.Constants;
 import com.gw.android.first_components.my_fragment.CRComponent;
 import com.gw.android.first_components.my_fragment.ComponentSimpleModel;
 
 public class PhotoSendGUI extends CRComponent {
-
-	// Browse files
-	public void chooseFile() {
-		Intent myIntent = new Intent(Intent.ACTION_PICK,
-				android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-		startActivityForResult(myIntent, PhotoUtils.SELECT_PICTURE);
+	String url; 
+	
+	private String getUrl() {
+		SharedPreferences testPrefs = getActivity()
+				.getApplication()
+				.getSharedPreferences("test_prefs", Context.MODE_PRIVATE);
+		return testPrefs.getString("base_url", "");
 	}
 	
 	/*TODO REQUEST - descricao abaixo
@@ -74,6 +77,54 @@ enviar aparentemente vazio, assim como os dois primeiros campos
 	 * (non-Javadoc)
 	 * @see android.support.v4.app.Fragment#onActivityResult(int, int, android.content.Intent)
 	 */
+	
+	String uploadRequest(String photopath) {
+		Request request = new Request(null, url, "post",
+				"__tagMgr.tags--" + "maisonteste, victorteste" +
+				"__user.id--" + "" +
+				"__photoRegister.id--" + "" +
+				"__photoRegister.name--" + "maisonteste" +
+				"__photoRegister.imageAuthor--" + "" +
+				"__photoRegister.state--" +
+				"__photoRegister.country--" + "Brasil" +
+				"__photoRegister.dataCriacao--" +
+				"__photoRegister.district--" +
+				"__photoRegister.workAuthor--" +
+				"__photoRegister.street--" +
+				"__photoRegister.workdate--" +
+				"__photoRegister.description--" +
+				"__photoRegister.allowCommercialUses--" + "YES" +
+				"__photoRegister.allowModifications--" + "YES" +
+				"__foto--" + "((IS_FILE_TAG))" + photopath +
+				"__terms--" + "read" +
+				"__enviar--" + "");
+		makeRequest(request);
+		return "";
+	}
+	
+	void saveInDatabase(String filepath, Context ctx) {
+		File f = new File(filepath);
+		Bitmap bitmap = PhotoUtils.resizeImage(f, 768);
+		Log.e("AFTER RESIZE width e height", bitmap.getWidth() + " - " + bitmap.getHeight());
+		
+		byte[] bArray = PhotoUtils.bitmapToByteArray(bitmap);
+		Photo photo = new Photo(ComponentSimpleModel.getUniqueId(ctx),
+				null, null, bArray, null, new Date());
+
+		PhotoDao photoDao = PhotoUtils.initPhotoDao(ctx);
+		long id = photoDao.insert(photo);
+		photoDao.getDatabase().close();
+
+		Toast.makeText(ctx, "Foto salva!", Toast.LENGTH_SHORT).show();
+		Log.v("Inseriu a foto no banco de dados!", "ID: " + id);
+	}
+
+	// Browse files
+	public void chooseFile() {
+		Intent myIntent = new Intent(Intent.ACTION_PICK,
+				android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+		startActivityForResult(myIntent, PhotoUtils.SELECT_PICTURE);
+	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -89,23 +140,10 @@ enviar aparentemente vazio, assim como os dois primeiros campos
 
 				int columnindex = cursor.getColumnIndex(filepathcolumn[0]);
 				String filepath = cursor.getString(columnindex);
-
 				cursor.close();
 
-				File f = new File(filepath);
-				Bitmap bitmap = PhotoUtils.resizeImage(f, 768);
-				Log.e("AFTER RESIZE width e height", bitmap.getWidth() + " - " + bitmap.getHeight());
-				
-				byte[] bArray = PhotoUtils.bitmapToByteArray(bitmap);
-				Photo photo = new Photo(ComponentSimpleModel.getUniqueId(ctx),
-						null, null, bArray, null, new Date());
-
-				PhotoDao photoDao = PhotoUtils.initPhotoDao(ctx);
-				long id = photoDao.insert(photo);
-				photoDao.getDatabase().close();
-
-				Toast.makeText(ctx, "Foto salva!", Toast.LENGTH_SHORT).show();
-				Log.v("Inseriu a foto no banco de dados!", "ID: " + id);
+				saveInDatabase(filepath, ctx);
+				uploadRequest(filepath);
 			}
 		}
 	}
@@ -122,6 +160,8 @@ enviar aparentemente vazio, assim como os dois primeiros campos
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		url = getUrl() + "/photo/"+ getCollabletId() +"/registra"; 
+		
 		View view = inflater.inflate(R.layout.photo_send, container, false);
 
 		Button save = (Button) view.findViewById(R.id.btnBrowse);
