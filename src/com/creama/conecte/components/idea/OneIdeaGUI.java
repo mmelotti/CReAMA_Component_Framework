e87cc4bade75131907ffbc53bd51be133818d6dc
maison <m_melotti@hotmail.com>
2014-05-30 16:02:47 -0300

    more stuff, going to the final

--- src/com/creama/conecte/components/idea/OneIdeaGUI.java
+++ src/com/creama/conecte/components/idea/OneIdeaGUI.java
@@ -20,29 +20,22 @@ import com.gw.android.first_components.my_fragment.CRComponent;
 @SuppressLint("ValidFragment")
 public class OneIdeaGUI extends CRComponent {
 
-	TextView title,descricao;
-	private String urlTest = "http://apiconecteideias.azurewebsites.net/ideias/searchById?id=";
-
-	private String urlFinal = "/ideias/searchById?id=";
-	
+	TextView title, descricao;
+	private String urlBase = "http://apiconecteideias.azurewebsites.net/";
+	private String urlFinal = "ideias/searchById?id=";
 	Long serverId;
-	
-	public OneIdeaGUI(Long serverId){
-		this.serverId=serverId;
-		Log.e("Request??","after construtor");
+
+	public OneIdeaGUI(Long serverId) {
+		this.serverId = serverId;
+		Log.e("Request??", "after construtor");
 	}
-	
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-
 		View view = inflater.inflate(R.layout.conecte_one_idea_comp, container,
 				false);
-		
-		
-		title=(TextView) view.findViewById(R.id.idea_titulo_one);
-		descricao=(TextView)view.findViewById(R.id.idea_body_one);
-		
-
+		title = (TextView) view.findViewById(R.id.idea_titulo_one);
+		descricao = (TextView) view.findViewById(R.id.idea_body_one);
 		AsyncRequestHandler mHandler = new AsyncRequestHandler() {
 			@Override
 			public void onSuccess(String response, Request request) {
@@ -55,61 +48,42 @@ public class OneIdeaGUI extends CRComponent {
 			}
 		};
 		setComponentRequestCallback(mHandler);
-		
-		
-		
-		
-
 		return view;
 	}
-
 	// http://conecteideias.azurewebsites.net/Ideia/Create
-
 	// http://apiconecteideias.azurewebsites.net/ideias/searchById?id=1067
-
 	// "X-ApiKey" e o value dele é 257F1D3C-57A0-4F34-A937-1538104E97FE
 
 	@Override
 	protected void onBind() {
 		// getConnectionManager().getCookiesInfo();
 		testRequest();
-
 	}
 
 	void testRequest() {
-
-		Request request = new Request(null, urlTest+serverId, "get", null);
+		Request request = new Request(null, urlBase + urlFinal + serverId,
+				"get", null);
 		String header[] = new String[2];
 		header[0] = "X-ApiKey";
 		header[1] = "257F1D3C-57A0-4F34-A937-1538104E97FE";
 		request.onlyOneHeader(header);
 		// request.setKeyValuePairs(keyValuePairs);
-
-		 makeRequest(request);
-
+		makeRequest(request);
 	}
 
 	public void atualizarAfterSucces(String r) {
 		try {
-
 			Log.i("onde ideia", " ...= ");
-
 			JSONObject ideasObject;
 			ideasObject = new JSONObject(r);
-
 			JSONArray nameArray = ideasObject.names();
-			JSONArray valArray = ideasObject.toJSONArray(nameArray);
-			JSONArray arrayResults = valArray.getJSONArray(0);
-			
 			Idea idea;
-			
 			Log.i("Parseando login antes for",
 					" ...= " + ideasObject.toString());
 			for (int j = 0; j < nameArray.length(); j++) {
-				//JSONObject oneIdea = arrayResults.getJSONObject(j);
-				String name=nameArray.getString(j);
-				Log.i("Parseando ideia dentro for",
-						" ....= " + name);
+				// JSONObject oneIdea = arrayResults.getJSONObject(j);
+				String name = nameArray.getString(j);
+				Log.i("Parseando ideia dentro for", " ....= " + name);
 				/*
 				 * JSONObject userObject = oneIdea.getJSONObject("user"); String
 				 * userName = userObject.get("name").toString(); Long idServ =
@@ -126,25 +100,19 @@ public class OneIdeaGUI extends CRComponent {
 				 * userName);
 				 */
 			}
-			idea=new Idea();
+			idea = new Idea();
 			idea.setTitle(ideasObject.getString("titulo"));
 			idea.setText(ideasObject.getString("descricao"));
 			setComponentGUI(idea);
 			// listview.setAdapter(new CommentAdapter(getActivity(), lista));
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		// Log.e("TEST R LOGIN",r);
-
 	}
 
-	
-	private void setComponentGUI(Idea idea){
-		
+	private void setComponentGUI(Idea idea) {
 		title.setText(idea.getTitle());
 		descricao.setText(idea.getText());
 	}
-	
+
 }
--- src/com/creama/conecte/components/user/ConecteLoginGUI.java
+++ src/com/creama/conecte/components/user/ConecteLoginGUI.java
@@ -1,7 +1,5 @@
 package com.creama.conecte.components.user;
 
-import java.util.Date;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,16 +19,13 @@ import com.gw.android.R;
 import com.gw.android.Utils.SuperToastUtils;
 import com.gw.android.components.connection_manager.AsyncRequestHandler;
 import com.gw.android.components.request.Request;
-import com.gw.android.first_components.my_components.comment.Comment;
-import com.gw.android.first_components.my_components.comment.CommentAdapter;
 import com.gw.android.first_components.my_fragment.CRComponent;
-import com.gw.android.first_components.my_fragment.ComponentSimpleModel;
 
 public class ConecteLoginGUI extends CRComponent {
 
 	TextView resultTxt;
 	String urlLogin;
-	private boolean loginVerified=false;
+	private boolean loginVerified = false;
 	EditText editLogin, editPassword;
 	Button btnSubmit;
 
@@ -40,23 +35,17 @@ public class ConecteLoginGUI extends CRComponent {
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-
 		View view = inflater.inflate(R.layout.conecte_login_comp, container,
 				false);
-
 		btnSubmit = (Button) view.findViewById(R.id.loginConecteSubmit);
 		editLogin = (EditText) view.findViewById(R.id.editConecteUser);
 		editPassword = (EditText) view.findViewById(R.id.editConectePass);
-
 		// TODO apagar isso aqui, é só pra teste
 		editLogin.setText("maisonmelotti@gmail.com");
 		editPassword.setText("minhasenha");
-
 		btnSubmit.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String login = editLogin.getText().toString();
-				String password = editPassword.getText().toString();
 				SuperToastUtils.showSuperToast(getActivity(),
 						SuperToast.BACKGROUND_GREENTRANSLUCENT,
 						"Efetuando login...");
@@ -85,20 +74,16 @@ public class ConecteLoginGUI extends CRComponent {
 	}
 
 	// http://conecteideias.azurewebsites.net/Ideia/Create
-
 	// http://apiconecteideias.azurewebsites.net/ideias/searchById?id=1067
-
 	// "X-ApiKey" e o value dele é 257F1D3C-57A0-4F34-A937-1538104E97FE
 
 	@Override
 	protected void onBind() {
 		// getConnectionManager().getCookiesInfo();
 		// testRequest();
-
 	}
 
 	void testRequest() {
-
 		Log.e("TEST R LOGIN", "fazendo request....");
 		Request request = new Request(null, urlVerifyUser
 				+ "maisonmelotti@gmail.com", "get", null);
@@ -107,7 +92,6 @@ public class ConecteLoginGUI extends CRComponent {
 		header[1] = "257F1D3C-57A0-4F34-A937-1538104E97FE";
 		request.onlyOneHeader(header);
 		// request.setKeyValuePairs(keyValuePairs);
-
 		makeRequest(request);
 
 	}
@@ -120,23 +104,15 @@ public class ConecteLoginGUI extends CRComponent {
 	}
 
 	public void atualizarAfterSucces(String r) {
-		if (r != null ) {
-			
+		if (r != null) {
 			try {
-
 				Log.i("onde ideia", " ...= ");
-
 				JSONObject userObject;
 				userObject = new JSONObject(r);
-
 				JSONArray nameArray = userObject.names();
-				JSONArray valArray = userObject.toJSONArray(nameArray);
-				JSONArray arrayResults = valArray.getJSONArray(0);
-
 				User user;
 				String email = "";
 				String pass = null;
-
 				// TODO i will get just the first email, should crash with more
 				// than one users email
 				JSONArray emailArray = userObject.getJSONArray("emails");
@@ -145,18 +121,14 @@ public class ConecteLoginGUI extends CRComponent {
 						" ...= " + emailObject.toString());
 				email = emailObject.getString("enderecoEmail");
 				pass = userObject.getString("senha");
-
 				Log.i("Parseando login antes for",
 						" ...= " + userObject.toString());
 				for (int j = 0; j < nameArray.length(); j++) {
 					// JSONObject oneIdea = arrayResults.getJSONObject(j);
 					String name = nameArray.getString(j);
-
 					if (nameArray.getString(j).equals("emails")) {
-
 					}
 					Log.i("LOGIN DENro names", " ....= " + name);
-
 				}
 				user = new User();
 				user.setName(userObject.getString("nome"));
@@ -166,26 +138,22 @@ public class ConecteLoginGUI extends CRComponent {
 				if (pass != null) {
 					verifyPassword(pass);
 				}
-
 			} catch (JSONException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} else { // a resposta veio vazia, nao tem user com o email
-
 		}
-
 	}
 
 	private void verifyPassword(String s) {
 		if (s.equals("minhasenha")) {
 			Log.i("TESTANDO LOGIN after senha", " ....= " + s);
-			loginVerified=true;
+			loginVerified = true;
 		}
 	}
 
-	public boolean isLoginVerified(){
+	public boolean isLoginVerified() {
 		return loginVerified;
 	}
-	
+
 }
