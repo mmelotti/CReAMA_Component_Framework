f594055821228d3527f55d9b3f5576a5af5c7ad2
maison <m_melotti@hotmail.com>
2014-06-10 13:50:29 -0300

    new!!

---
+++ src/com/gw/android/first_components/my_fragment/CRLoginInterface.java
@@ -0,0 +1,11 @@
+package com.gw.android.first_components.my_fragment;
+
+public interface CRLoginInterface {
+	public boolean isLoginOk=false;
+
+	public boolean isLoginOk();
+
+	void verifyPassword(String s);
+	
+	
+}
