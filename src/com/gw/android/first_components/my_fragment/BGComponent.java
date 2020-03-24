3ad3ce8a0b15b3959e91cb5e288c9261c24da63d
Maison Melotti <maisonmelotti@gmail.com>
2020-03-24 19:27:36 -0300

    Update BGComponent.java

--- src/com/gw/android/first_components/my_fragment/BGComponent.java
+++ src/com/gw/android/first_components/my_fragment/BGComponent.java
@@ -3,6 +3,4 @@ package com.gw.android.first_components.my_fragment;
 public abstract class BGComponent extends GenericComponent implements
 		CRComponent.RequestListener {
 
-	// test, ta funcionando aqui
-
 }
